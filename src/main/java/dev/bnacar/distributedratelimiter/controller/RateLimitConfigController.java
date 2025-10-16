b65e8bce05364c99a60394b7a5f9d1c1853b5ff1
uppnrise <mtbnacar@gmail.com>
2025-10-16 22:10:14 +0200

    refactor: clean up rate limit configuration controller
    - Replace wildcard imports with specific Spring annotations
    - Remove unused imports (Parameter, ExampleObject, ApiResponses, Map)
    - Remove unnecessary @Autowired annotation
    - Streamlines configuration controller dependencies

--- src/main/java/dev/bnacar/distributedratelimiter/controller/RateLimitConfigController.java
+++ src/main/java/dev/bnacar/distributedratelimiter/controller/RateLimitConfigController.java
@@ -7,18 +7,18 @@ import dev.bnacar.distributedratelimiter.ratelimit.ConfigurationResolver;
 import dev.bnacar.distributedratelimiter.ratelimit.RateLimiterConfiguration;
 import dev.bnacar.distributedratelimiter.ratelimit.RateLimiterService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller for managing rate limiter configuration.
@@ -32,7 +32,6 @@ public class RateLimitConfigController {
     private final ConfigurationResolver configurationResolver;
     private final RateLimiterService rateLimiterService;
 
-    @Autowired
     public RateLimitConfigController(RateLimiterConfiguration configuration,
                                    ConfigurationResolver configurationResolver,
                                    RateLimiterService rateLimiterService) {
