a676182453a80e2f516ba03ba80f61943beb41c5
uppnrise <mtbnacar@gmail.com>
2025-10-16 22:10:03 +0200

    refactor: clean up admin and performance controllers
    - Replace wildcard imports with specific Spring annotations
    - Remove unused imports (ExampleObject, PostMapping, etc.)
    - Remove unnecessary @Autowired annotations
    - Improves import clarity and reduces unused dependencies

--- src/main/java/dev/bnacar/distributedratelimiter/controller/AdminController.java
+++ src/main/java/dev/bnacar/distributedratelimiter/controller/AdminController.java
@@ -11,14 +11,18 @@ import dev.bnacar.distributedratelimiter.ratelimit.RateLimiterService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import java.util.List;
@@ -37,7 +41,6 @@ public class AdminController {
     private final RateLimiterConfiguration configuration;
     private final ConfigurationResolver configurationResolver;
 
-    @Autowired
     public AdminController(RateLimiterService rateLimiterService,
                           RateLimiterConfiguration configuration,
                           ConfigurationResolver configurationResolver) {
